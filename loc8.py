15c2cfe14c908898f75d949d69d1730ad09ae909
Harrison Grodin <grodinh@winchesterthurston.org>
2017-10-20 11:16:23 -0400

    Fix entire learning process
    Major refactor!

--- loc8.py
+++ loc8.py
@@ -1,14 +1,12 @@
 """Loc8."""
 
-from collections import deque
-
 import matplotlib.pyplot as plt
 import numpy as np
 import rl.core as krl
-from keras.optimizers import Adam
-from sklearn.cluster import MiniBatchKMeans
 
 from model import model
+
+LAYERS = 3
 
 
 class Loc8World:
@@ -18,119 +16,70 @@ class Loc8World:
 
         assert shape, "World must have a defined shape"
 
-        self.explored = np.zeros((0, len(shape)), dtype=np.float)
         self.shape = np.array(shape)
 
-        if start is None:
-            start = (0,) * len(shape)
-        self.position = start
+        self.position = np.array(start) if start else np.zeros(len(shape))
+        self.array = np.zeros((*self.shape, LAYERS))
 
-        self.goal = goal if goal is not None else self.random_goal()
+        self._goal = goal
+        self.goal = self._goal or self.random_goal()
 
-    @property
-    def position(self):
-        """Current explorer position."""
-        return self.explored[-1]
+    def move(self, delta):
+        copy = self.position.copy()
+        self.position += delta
+        self.position = self.position.clip(0, self.shape - 1)
+        return copy
 
-    @position.setter
-    def position(self, value):
-        """Set explorer position."""
-        self.explored = np.vstack([self.explored, value])
-
-    @property
-    def array(self):
-
-        array = np.zeros(self.shape)
-
-        for coord in np.floor(self.explored).astype(np.int):
-            if (coord < self.shape).all():
-                array[tuple(coord)] = 1
-
-        return array
+    def coords(self, position=None):
+        if position is None:
+            position = self.position
+        return tuple(position.astype(np.int))
 
     @property
     def distance_to_goal(self):
         """Return the current distance to the goal."""
-        return np.sqrt(((self.goal - self.position)**2).sum())
+        return np.linalg.norm(self.goal - self.position)
 
     def random_goal(self):
         """Choose a random goal."""
-        self.goal = np.array([np.random.randint(axis) for axis in self.shape])
-        return self.goal
-
-    def distances(self, center):
-        """Calculate distances from all explored points to a given center."""
-        return np.sqrt(((center - self.explored)**2).sum(axis=1))
-
-    def far_points(self):
-
-        results = []
-
-        for coords in np.argwhere(self.array == 0):
-            results.append((coords, self.distances(coords).min()))
-
-        largest = max(results, key=lambda a: a[1])[1]
-        filtered = filter(lambda a: a[1] >= largest / 2, results)
-
-        far_points = np.array([point[0] for point in filtered])
-
-        return far_points
-
-    def key_points(self, n_points):
-        """Use Mini-Batch K-Means clustering to develop :data:`n_clusters`
-        clusters that are representative of the empty space in the
-        :class:`Loc8World`."""
-
-        far_points = self.far_points()
-
-        if len(far_points) < n_points:
-            far_points = np.tile(
-                far_points.T,
-                int(np.ceil(n_points / len(far_points)))
-            ).T[:n_points]
-
-        centers = MiniBatchKMeans(
-            n_clusters=n_points
-        ).fit(far_points).cluster_centers_
-
-        return centers
+        return np.array([np.random.randint(axis) for axis in self.shape])
 
 
 class Loc8Env(krl.Env):
     """Loc8 environment."""
 
-    def __init__(self, *shape, n_points, vision=3, moving_average_len=3):
+    def __init__(self, *shape, vision=2, goal=None):
 
-        self.world = Loc8World(*shape)
-        self.n_points = n_points
+        self.world = Loc8World(*shape, goal=goal)
         self.vision = vision
 
-        self.steps = 0
-        self.observation = self.observe()
-
-        self.choices = deque(maxlen=moving_average_len)
+        self.choices = np.array(
+            np.meshgrid(*[[-1, 0, 1]] * len(shape))
+        ).T.reshape(-1, len(shape))
 
     def observe(self):
         """Retrieve observations for reinforcement learning."""
-        return self.world.array.reshape(5, 5, 5, 5).mean(axis=(3, 1)).flatten()
+        coords = self.world.coords()
+
+        self.world.array[(*coords, 1)] = 1
+        world = self.world.array.copy()
+
+        world[(*coords, 2)] = 1
+
+        return world
 
     def step(self, action):
 
-        action = np.clip(action, 0, 1) * self.world.shape
-        self.choices.append(action)
+        reward = 0
 
-        moving_towards = np.mean(self.choices, axis=0)
-        delta = moving_towards - self.world.position
-        if not np.all(delta == 0):
-            self.world.position += delta / np.sqrt((delta**2).sum())
+        choice = self.choices[action]
+        self.world.move(choice)
 
         observation = self.observe()
 
         done = self.world.distance_to_goal < self.vision
 
-        reward = np.sum(observation - self.observation) * 10
-        self.observation = observation
-        self.steps += 1
+        reward = 100 if done else 0
 
         # observation, reward, done, info
         return observation, reward, done, {}
@@ -141,14 +90,12 @@ class Loc8Env(krl.Env):
         plt.xlim(0, self.world.shape[0])
         plt.ylim(0, self.world.shape[1])
 
-        self.world = Loc8World(*self.world.shape, goal=(5, 20))
+        self.world = Loc8World(*self.world.shape, goal=self.world._goal)
 
         goal = self.world.goal
         plt.plot([goal[0]], [goal[1]], 'g^', markersize=20)
 
-        self.steps = 0
-        self.observation = self.observe()
-        return self.observation
+        return self.observe()
 
     ########
 
@@ -168,24 +115,32 @@ class Loc8Env(krl.Env):
 ###
 
 
-def run(*shape, n_points, actor_dense=1, critic_dense=1, **kwargs):
+def run(*shape, dense_layers=16, **kwargs):
     """Run reinforcement learning algorithm with a given world size."""
 
-    env = Loc8Env(*shape, n_points=n_points, **kwargs)
-    nb_actions = len(env.world.shape)
-    observation_shape = (25,)  # (np.prod(env.world.shape),)
+    env = Loc8Env(*shape, **kwargs)
+    nb_actions = len(env.choices)
+    observation_shape = (*env.world.shape, LAYERS)
 
     agent = model(
         nb_actions, observation_shape,
-        actor_dense=actor_dense, critic_dense=critic_dense
+        dense_layers=dense_layers
     )
-    agent.compile(Adam(lr=.001, clipnorm=1.), metrics=['mae'])
 
-    agent.fit(env, nb_steps=50000, visualize=False, verbose=2)
+    agent.fit(
+        env,
+        nb_steps=50000,
+        nb_max_episode_steps=100,
+        visualize=True,
+        verbose=1,
+    )
 
-    # agent.save_weights("loc8_weights.h5f", overwrite=True)
-
-    agent.test(env, nb_episodes=50, visualize=True)
+    agent.test(
+        env,
+        nb_episodes=50,
+        nb_max_episode_steps=100,
+        visualize=True,
+    )
 
 if __name__ == "__main__":
-    run(25, 25, n_points=5, moving_average_len=10)
+    run(10, 10, goal=(2, 8))
--- model.py
+++ model.py
@@ -1,46 +1,34 @@
-from keras.layers import Dense, Flatten, Input, concatenate
-from keras.models import Model, Sequential
-from rl.agents import DDPGAgent
+from keras.layers import Conv3D, Dense, Flatten
+from keras.models import Sequential
+from keras.optimizers import Adam
+from rl.agents import DQNAgent
 from rl.memory import SequentialMemory
-from rl.random import OrnsteinUhlenbeckProcess
+from rl.policy import BoltzmannQPolicy
 
 
-def model(nb_actions, observation_shape, *,
-          actor_dense=1, critic_dense=1, summary=True):
+def model(nb_actions, observation_shape, *, dense_layers=16, summary=True):
 
-    actor = Sequential()
-    actor.add(Flatten(input_shape=(1,) + observation_shape))
-    for _ in range(actor_dense):
-        actor.add(Dense(16, activation="relu"))
-    actor.add(Dense(nb_actions, activation="linear"))
-    if summary:
-        actor.summary()
+    model = Sequential()
+    model.add(Conv3D(
+        64,
+        kernel_size=(1, 10, 10),
+        activation='relu',
+        input_shape=(1,) + observation_shape
+    ))
+    model.add(Flatten())
+    for _ in range(dense_layers):
+        model.add(Dense(256, activation='relu'))
+    model.add(Dense(nb_actions, activation='softmax'))
 
-    action_input = Input(shape=(nb_actions,), name="action_input")
-    observation_input = Input(
-        shape=(1,) + observation_shape, name="observation_input")
-    output = concatenate([action_input, Flatten()(observation_input)])
-    for _ in range(critic_dense):
-        output = Dense(16, activation="relu")(output)
-    output = Dense(1, activation="linear")(output)
-    critic = Model(inputs=[action_input, observation_input], outputs=[output])
-    if summary:
-        critic.summary()
+    model.summary()
 
-    memory = SequentialMemory(limit=10000, window_length=1)
-    random_process = OrnsteinUhlenbeckProcess(
-        size=nb_actions, theta=.15, mu=0, sigma=.1)
-    agent = DDPGAgent(
+    agent = DQNAgent(
+        model=model,
         nb_actions=nb_actions,
-        actor=actor,
-        critic=critic,
-        critic_action_input=action_input,
-        memory=memory,
-        nb_steps_warmup_critic=50,
-        nb_steps_warmup_actor=100,
-        random_process=random_process,
-        gamma=.99,
-        target_model_update=1e-3
+        memory=SequentialMemory(limit=10000, window_length=1),
+        nb_steps_warmup=100,
+        policy=BoltzmannQPolicy()
     )
+    agent.compile(Adam(lr=1e-3), metrics=['mse'])
 
     return agent
